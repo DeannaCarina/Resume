13abc2904400878f8b966ee3af029694a5464343
DeannaCarina <deannacarina@hotmail.com>
2022-07-03 05:37:48 +0000

    update: Resume and interests

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,11 +10,15 @@ function initMap() {
     var labels = " ";
 
     var locations = [
-        { lat: 53.476990, lng: -2.243460 },//Manchester
-        { lat: 53.763622, lng: -2.707455 },//Preston
-        { lat: 53.405177, lng: -2.992213 },//Liverpool
+        { lat: 53.735388, lng: -2.459979 },//RBH
+        { lat: 53.750324, lng: -2.488124 },//BCW
+        { lat: 53.759647, lng: -2.367326 },//AVH
+        { lat: 53.809569, lng: -2.227980 },//BGH
+        { lat: 53.700433, lng: -2.280974 },//RPHC
+        { lat: 53.883147, lng: -2.374288 },//CCH
+        { lat: 53.838597, lng: -2.211329 },//PCH
     ];
-
+    
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
