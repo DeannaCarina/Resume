c7b8494cba985c9a0aa4a3b56d4a30c74fc1efd5
DeannaCarina <deannacarina@hotmail.com>
2022-07-03 06:30:47 +0000

    add: Settings for contact form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "template_DeannaCarina", {
+    emailjs.send("service_ugv6x4n", "template_deblqlp", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.email.value,
         "form_subject": contactForm.subject.value,
@@ -14,5 +14,6 @@ function sendMail(contactForm) {
             console.log("FAILED", error);
         }
     );
-    return false;
+    document.getElementById('fillForm').reset(); // Reset form once the message has been sent
+    return false; // This is to prevent the page from refreshing when you submit your form 
 }
