aea27399639518439ceb2354132c523d78088c35
DeannaCarina <deannacarina@hotmail.com>
2023-01-21 10:18:28 +0000

    Fix of 405 error contact form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,26 +1,19 @@
 
 
-document.getElementById('fillForm').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    emailjs.sendForm('service_2widl53', 'template_DeannaCarina', this,
-    {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "form_subject": contactForm.subject.value,
-        "form_message": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            alert('Your email has been sent.')
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-);
-    document.getElementById('fillForm').reset(); // Reset form once the message has been sent
-    return false; // This is to prevent the page from refreshing when you submit your form 
-})
 
-  
+window.onload = function() {
+    document.getElementById('fillForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+                
+        emailjs.sendForm('service_2widl53', 'template_DeannaCarina', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+        document.getElementById('fillForm').reset(); // Reset form once the message has been sent
+        return false; // This is to prevent the page from refreshing when you submit your form 
+    });    
+}
+window.location.replace("https://deannacarina.github.io/Resume/thanks.html");
