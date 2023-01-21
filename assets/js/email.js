00687ada7108893198af85884b997b7ec024c49a
DeannaCarina <deannacarina@hotmail.com>
2023-01-21 10:21:03 +0000

    fix redirect issue from contact form

--- assets/js/email.js
+++ assets/js/email.js
@@ -9,11 +9,11 @@ window.onload = function() {
         emailjs.sendForm('service_2widl53', 'template_DeannaCarina', this)
             .then(function() {
                 console.log('SUCCESS!');
+                document.getElementById('fillForm').reset(); // Reset form once the message has been sent
+                window.location.replace("https://deannacarina.github.io/Resume/thanks.html");
             }, function(error) {
                 console.log('FAILED...', error);
             });
-        document.getElementById('fillForm').reset(); // Reset form once the message has been sent
-        return false; // This is to prevent the page from refreshing when you submit your form 
     });    
 }
-window.location.replace("https://deannacarina.github.io/Resume/thanks.html");
+
